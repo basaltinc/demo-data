291b88dcf14a22566ff22e7295f7dc9e1bbd5c8d
Evan Lovely <evanlovely@gmail.com>
2018-07-31 16:06:40 -0700

    adding stock images

--- src/data.js
+++ src/data.js
@@ -15,6 +15,17 @@ const images = [
   'imagePlaceholderA.png',
   'imagePlaceholderB.png',
   'imagePlaceholderC.png',
+  'basalt-brand-stock/adam-birkett-182279.jpg',
+  'basalt-brand-stock/annie-spratt-294450.jpg',
+  'basalt-brand-stock/basalt2bw--cropped.jpg',
+  'basalt-brand-stock/basalt4bw.jpg',
+  'basalt-brand-stock/chris-schog-271197.jpg',
+  'basalt-brand-stock/clarisse-meyer-304306.jpg',
+  'basalt-brand-stock/domenico-loia-310197.jpg',
+  'basalt-brand-stock/julentto-photography-184055.jpg',
+  'basalt-brand-stock/milada-vigerova-66601.jpg',
+  'basalt-brand-stock/parker-byrd-139348.jpg',
+  'basalt-brand-stock/rawpixel-com-191102.jpg',
 ];
 
 /** @type {string[]} */
